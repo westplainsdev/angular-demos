b1db59fb0b262756db018d31bda99eb49d5294f6
Chris Berry <chris.berry07@gmail.com>
2013-07-24 15:24:14 -0400

    Fixed showdown directive for dynamic content

--- js/directives.js
+++ js/directives.js
@@ -11,15 +11,33 @@ angular.module('myApp.directives', []).
   }]);
   
   
-angular.module('myApp.directives', []).
- directive('markdown', function () {
-    var converter = new Showdown.converter();
-    return {
-        restrict: 'C',
-        link: function (scope, element, attrs) {
-            var htmlText = converter.makeHtml(element.text());
-            element.html(htmlText);
-        }
-    };
+// angular.module('myApp.directives', []).
+//  directive('markdown', function () {
+//     var converter = new Showdown.converter();
+//     return {
+//         restrict: 'C',
+//         link: function (scope, element, attrs) {
+//             var htmlText = converter.makeHtml(element.text());
+//             element.html(htmlText);
+//         }
+//     };
 
-});
+// });
+
+  angular.module('myApp.directives').directive('markdown', function () {
+      var converter = new Showdown.converter();
+      return {
+          restrict: 'C',
+          require: 'ngModel',
+          scope: {
+              model: '=ngModel'
+          },
+          link: function (scope, elem, attrs) {
+              scope.$watch('model', function () {
+                  // do your stuff with the model
+                  var htmlText = converter.makeHtml(scope.model);                  
+                  elem.html(htmlText);
+              }, true);
+          }
+      };
+  });
